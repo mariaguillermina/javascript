61b8956b86a81db69f31e2b31011d89a2f64b351
Guille <guillerminaup@gmail.com>
2021-11-27 07:15:45 -0500

    javascript

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+
+nombre = prompt('Ingresa tu Nombre')
+if (!nombre) {
+    alert('El nombre es obligatorio')
+
+}else {
+}
+console.log(nombre.toUpperCase())
+console.log(nombre[0])
+
+console.log(nombre)
+
+apellido = prompt('Ingresa tu Apellido')
+if (!apellido) {
+    alert('El apellido es obligatorio')
+
+}else {
+}
+console.log(apellido.toUpperCase())
+console.log(apellido[0])
+
+console.log(apellido)
+
+
+edad = prompt('Ingresa tu edad');
+if (!edad >18){
+    alert('Debes ser mayor de 18 años para ingresar a este sitio')
+}else{
+}
