99df5b7c9e87afce0943dc7eab3a35527eb3022a
Guille <guillerminaup@gmail.com>
2021-12-02 05:04:47 -0500

    fix

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
+let nombre = prompt("Ingresa tu Nombre");
 
-nombre = prompt('Ingresa tu Nombre')
 if (!nombre) {
     alert('El nombre es obligatorio')
 
@@ -11,7 +11,8 @@ console.log(nombre[0])
 
 console.log(nombre)
 
-apellido = prompt('Ingresa tu Apellido')
+let apellido = prompt("Ingresa tu Apellido");
+
 if (!apellido) {
     alert('El apellido es obligatorio')
 
@@ -22,9 +23,9 @@ console.log(apellido[0])
 
 console.log(apellido)
 
+let edad= prompt("Ingresa tu Edad");
+edad = parseInt(edad) 
 
-edad = prompt('Ingresa tu edad');
-if (!edad >18){
+if (edad <=18){
     alert('Debes ser mayor de 18 años para ingresar a este sitio')
-}else{
 }
